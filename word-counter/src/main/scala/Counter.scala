b94a290c20bf6bc6d7d3c78202c6186f6838f241
Anzori (Nika) Gurchumelia <aghurtchumelia@Anzoris-MacBook-Pro.local>
2023-06-27 22:50:00 +0200

    implement Counter

--- word-counter/src/main/scala/Counter.scala
+++ word-counter/src/main/scala/Counter.scala
@@ -1,7 +1,17 @@
 trait Counter {
-  def apply(input: String): CountResult
+  def apply(input: String): Option[CountResult]
 }
 
 object Counter {
-  def apply(userOption: UserOption): Counter = (input: String) => ???
+  def apply(userOption: UserOption): Counter = (input: String) =>
+    CountResult(
+      userOption = userOption,
+      amount = userOption match {
+        case UserOption.Byte => input.getBytes.sum
+        case UserOption.Character => input.split(" ").map(_.length).sum
+        case UserOption.Word => input.split(" ").length
+        case UserOption.Line => input.split("\n").length
+      },
+    )
+
 }
--- word-counter/src/main/scala/Main.scala
+++ word-counter/src/main/scala/Main.scala
@@ -1,5 +1,16 @@
 object Main {
 
-  def main(args: Array[String]): Unit = {}
+  def main(args: Array[String]): Unit =
+    args.toList match {
+      case s"$Hyphen$userOption" :: fileName :: Nil =>
+        ???
+
+      case Cat :: fileName :: Pipe :: programName :: userOption :: Nil =>
+        ???
+    }
+
+  private val Pipe = "|"
+  private val Cat = "cat"
+  private val Hyphen = "-"
 
 }
