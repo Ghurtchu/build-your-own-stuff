1fc12a89bc53d74331ec72b92501746865eab5db
Anzori (Nika) Gurchumelia <aghurtchumelia@Anzoris-MacBook-Pro.local>
2023-06-28 00:04:28 +0200

    fold UserOption

--- word-counter/src/main/scala/Main.scala
+++ word-counter/src/main/scala/Main.scala
@@ -5,19 +5,19 @@ object Main {
   def main(args: Array[String]): Unit =
     args.toList match {
       case s"-$userOption" :: fileName :: Nil =>
-        UserOption.fromString(userOption) match {
-          case Some(option) =>
+        UserOption
+          .fromString(userOption)
+          .fold(println(s"unrecognized option: $userOption")) { option =>
             val counter = Counter.apply(option)
             val input = Files.readString(Path.of(fileName))
             val result = counter.count(input)
+
             println(result)
-          case None =>
-            println(s"unrecognized option: $userOption")
-        }
+          }
 
       case Cat :: fileName :: Pipe :: programName :: userOption :: Nil =>
         ???
-      case _ => println("XD")
+      case _ => println("Incorrect usage")
     }
 
   private val Pipe = "|"
