a362a5d20617432139e3928a072ba408d9887155
Anzori (Nika) Gurchumelia <anzori.gurchumelia@gau.edu.ge>
2023-06-28 11:15:05 +0200

    define Command.Word case

--- word-counter/src/main/scala/services/Counter.scala
+++ word-counter/src/main/scala/services/Counter.scala
@@ -2,6 +2,9 @@ package services
 
 import domain.{Amount, Command, CountResult}
 
+import java.nio.charset.Charset
+import java.util.regex.Pattern
+
 trait Counter {
   def count(input: String): CountResult
 }
@@ -15,9 +18,17 @@ object Counter {
 
   private def defineAmount(command: Command, input: String): BigInt =
     command match {
-      case Command.Byte => input.getBytes.sum
+      case Command.Byte => input.getBytes.length
       case Command.Character => input.length
-      case Command.Word => (input split " ").length
+      case Command.Word =>
+        val nonLetterChars = input.toSet.filter(char => !Character.isLetter(char))
+        val splitted = (input split "\n")
+          .flatMap(_ split "\\s")
+
+        nonLetterChars.foldLeft(splitted) { (acc, char) =>
+          acc.flatMap(_.split(char))
+        } count { text => Pattern.matches("^[a-zA-Z]+", text) }
+
       case Command.Line => (input split "\n").length
     }
 }
