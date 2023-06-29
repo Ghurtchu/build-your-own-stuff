09b8c779780ab335b550c660470b7bbd01015a8d
Anzori (Nika) Gurchumelia <anzori.gurchumelia@gau.edu.ge>
2023-06-30 00:17:42 +0200

    add tests

--- word-counter/src/test/scala/services/ParseCmdAndThenCountWordsSpec.scala
+++ word-counter/src/test/scala/services/ParseCmdAndThenCountWordsSpec.scala
@@ -2,8 +2,9 @@ package services
 
 import domain.{Amount, Command, CountResult}
 import munit.FunSuite
-import services.ParseCmdAndThenCountWords.Error.UnknownCommand
+import services.ParseCmdAndThenCountWords.Error._
 
+import java.nio.file.NoSuchFileException
 import scala.util.Try
 
 class ParseCmdAndThenCountWordsSpec extends FunSuite {
@@ -20,6 +21,22 @@ class ParseCmdAndThenCountWordsSpec extends FunSuite {
     val expected = CountResult(Command.Line, Amount(3))
 
     assertEquals(actual, Right(expected))
+  }
+
+  test("catch error in terms of non existing file") {
+    val loadInput = scala.util.Failure(new NoSuchFileException(""))
+    val filepath = "~/imaginary.txt"
+    val actual = ParseCmdAndThenCountWords.fromFile(filepath)("l", loadInput)
+
+    assertEquals(actual, Left(FileNotFound(filepath)))
+  }
+
+  test("return Unknown error since OutOfMemoryError() is not in our control") {
+    val loadInput = scala.util.Failure(new OutOfMemoryError())
+    val filepath = "~/imaginary.txt"
+    val actual = ParseCmdAndThenCountWords.fromFile(filepath)("l", loadInput)
+
+    assertEquals(actual, Left(Unknown))
   }
 
   test("return Left(UnkownCommand) since command is unknown") {
