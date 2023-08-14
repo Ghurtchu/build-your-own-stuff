b59934c4d8e4b9ac3a0d8b77511ea582530fc54d
Anzori (Nika) Gurchumelia <aghurtchumelia@Anzoris-MacBook-Pro.local>
2023-08-14 22:38:26 +0200

    polish Main

--- cut/src/main/scala/Main.scala
+++ cut/src/main/scala/Main.scala
@@ -1,4 +1,4 @@
-import domain.{Delimiter, Header, Regex}
+import domain.{Dataframe, Delimiter, Header, Regex}
 import services.{DataframeParser, NumbersParser}
 
 import java.nio.file.{Files, Path}
@@ -10,39 +10,33 @@ object Main {
       case s"-f$numbers" :: filename :: Nil =>
         val input = loadInputOrFail(filename)
         val dataframe = DataframeParser.ofTab(input)
-        Regex.fromNumbers(numbers) match {
-          case Some(regex) =>
-            NumbersParser
-              .fromRegex(regex)(numbers)
-              .map(_.map(dataframe.getColumnByIndex))
-              .fold(println("error"))(_.foreach(println))
-          case None =>
-            Try(numbers.toInt).toOption
-              .map(dataframe.getColumnByIndex)
-              .fold(println("Input was not a number"))(
-                _.fold(e => println(e.msg), println),
-              )
-        }
+        parseNumbersAndlogResult(numbers, dataframe)
       case s"-f$numbers" :: s"-d$delimiterWithValue" :: filename :: Nil =>
         val input = loadInputOrFail(filename)
         val delimiter = Delimiter
           .fromString(delimiterWithValue.tail)
           .getOrElse(Delimiter.Tab)
         val dataframe = DataframeParser.of(delimiter)(input)
-        Regex.fromNumbers(numbers) match {
-          case Some(regex) =>
-            NumbersParser
-              .fromRegex(regex)(numbers)
-              .map(_.map(dataframe.getColumnByIndex))
-              .fold(println("error"))(_.foreach(println))
-          case None =>
-            Try(numbers.toInt).toOption
-              .map(dataframe.getColumnByIndex)
-              .fold(println("Input was not a number"))(
-                _.fold(e => println(e.msg), println),
-              )
-        }
+        parseNumbersAndlogResult(numbers, dataframe)
       case _ => println("Incorrect usage, please refer to manual")
+    }
+
+  private def parseNumbersAndlogResult(
+    numbers: String,
+    dataframe: Dataframe,
+  ): Unit =
+    Regex.fromNumbers(numbers) match {
+      case Some(regex) =>
+        NumbersParser
+          .fromRegex(regex)(numbers)
+          .map(_.map(dataframe.getColumnByIndex))
+          .fold(println("error"))(_.foreach(println))
+      case _ =>
+        Try(numbers.toInt).toOption
+          .map(dataframe.getColumnByIndex)
+          .fold(println("Input was not a number"))(
+            _.fold(e => println(e.msg), println),
+          )
     }
 
   private def loadInputOrFail(filename: String): String =
