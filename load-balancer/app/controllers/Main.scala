2dd4aac704c9a7849958b6395995dc4b6aed56ca
Anzori (Nika) Gurchumelia <aghurtchumelia@Anzoris-MacBook-Pro.local>
2023-08-24 21:16:08 +0200

    load-balancer start

---
+++ load-balancer/app/controllers/Main.scala
@@ -0,0 +1,38 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class Main @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+  /**
+   * Create an Action to render an HTML page.
+   *
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def index: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    println(s"Received request from $request")
+    println(s"${request.method}")
+    println(s"Host: ${request.host}")
+    println(s"User-Agent: ${request.headers}")
+
+
+    NoContent
+  }
+
+  def getNumbers: Action[AnyContent] = Action { _ =>
+    Ok {
+      (1 to 10)
+        .map(_.toString)
+        .reduce(_ concat "\n" concat _)
+    }
+  }
+}
---
+++ load-balancer/build.sbt
@@ -0,0 +1,17 @@
+name := """load-balancer"""
+organization := "com.ghurtchu"
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+scalaVersion := "2.13.11"
+
+libraryDependencies += guice
+libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test
+
+// Adds additional packages into Twirl
+//TwirlKeys.templateImports += "com.ghurtchu.controllers._"
+
+// Adds additional packages into conf/routes
+// play.sbt.routes.RoutesKeys.routesImport += "com.ghurtchu.binders._"
