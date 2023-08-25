0d2041144b44d0128c3ce021cc09caf2d559cdb6
Anzori (Nika) Gurchumelia <aghurtchumelia@Anzoris-MacBook-Pro.local>
2023-08-25 18:28:14 +0200

    recover future wit Ok("")

--- load-balancer/app/controllers/LoadBalancer.scala
+++ load-balancer/app/controllers/LoadBalancer.scala
@@ -1,24 +1,15 @@
 package controllers
 
-import akka.stream.ConnectionException
 import domain.Servers
-import play.api.PlayException
 
 import javax.inject._
 import play.api.mvc._
 import play.api.libs.ws._
-import play.api.libs.ws.ahc.AhcWSResponse
 
-import scala.concurrent.{ExecutionContext, Future}
-import scala.concurrent.impl.Promise
-import scala.util.{Failure, Success, Try}
+import scala.concurrent.ExecutionContext
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
-class LoadBalancer @Inject()(ws: WSClient, val controllerComponents: ControllerComponents) extends BaseController {
+final case class LoadBalancer @Inject()(ws: WSClient, controllerComponents: ControllerComponents) extends BaseController {
 
   implicit val ec: ExecutionContext = controllerComponents.executionContext
 
@@ -28,15 +19,7 @@ class LoadBalancer @Inject()(ws: WSClient, val controllerComponents: ControllerC
       "http://localhost:9002/all",
       "http://localhost:9003/all"
   )
-
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index: Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
+  def run: Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
     val url = servers.next
 
     ws.url(url)
